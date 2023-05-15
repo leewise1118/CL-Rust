cd226a82a2d25cb54707fd14309e6e058aaa4acb
lee <1076774025@qq.com>
2023-05-15 23:38:15 +0800

    catr-TODO

---
+++ catr/src/lib.rs
@@ -0,0 +1,50 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("lee <1076774025@qq.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .long("number")
+                .help("number all output lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("number nonempty output lines, overrides -n")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
